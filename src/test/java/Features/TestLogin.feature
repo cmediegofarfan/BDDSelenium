d2f8d94feef6d28e8608e8e22d8be3cc65d85b4f
Diego Farfan <diego.farfan@gotocme.com>
2018-05-10 18:04:16 -0300

    Commit with Navi's changes

--- src/test/java/Features/TestLogin.feature
+++ src/test/java/Features/TestLogin.feature
@@ -2,13 +2,11 @@ Feature: Login in ea.com
 
   Scenario Outline: Login with several account Outline scenario
     Given I navigate fb page
-    And I enter <username> and <pass>
+    And I enter <username>
     And I verify the count of my salary digits for rs 10000
     And I click Login button
     Then I should see my personal wall
 
     Examples:
-      |username|pass|
-      |admin|1234|
-      |diego|pass1234|
-      |root|root|
+      |username|
+      |boyanjavier@gmail.com|
--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -4,6 +4,7 @@ import Base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Hook extends BaseUtil {
@@ -18,8 +19,8 @@ public class Hook extends BaseUtil {
     public void InitializeTest (){
         //using a dummy webdriver
         //base.StepInfo = "FirefoxWebDriver";
-        System.setProperty("webdriver.firefox.marionette", "D:\\libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        base.Driver = new ChromeDriver();
         System.out.println("Opening the browser: Firefox\n");
 
     }
